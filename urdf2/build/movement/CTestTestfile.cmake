67a4a82bbb39a49ee56732dbc004eeadda716a23
BobbieZain <boweizhang2001@gmail.com>
2024-05-07 13:42:56 +0100

    gitignore added

--- urdf2/build/movement/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/boz/MEng-project-9-2023/simulation/urdf2/movement
-# Build directory: /home/boz/MEng-project-9-2023/simulation/urdf2/build/movement
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
